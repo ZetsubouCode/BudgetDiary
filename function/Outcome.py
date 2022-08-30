be8d23d92a3fe7f9dcfc6b4a372ee0efa2408e0c
ZetsubouCode <kignatius80@gmail.com>
2022-08-31 06:25:36 +0700

    Add null filter

--- function/Outcome.py
+++ function/Outcome.py
@@ -1,6 +1,6 @@
 from controller.Outcome import Outcome as OutcomeController
 from controller.Transaction import Transaction as TransactionController
-from datetime import datetime as date
+from datetime import timedelta,datetime as date
 class Outcome:
     async def add(msg:str):
         data = msg.split("-")
@@ -11,18 +11,26 @@ class Outcome:
     async def get_daily_expense(msg):
         data_date = date.strptime(msg, "%Y-%m-%d")
         outcome = await OutcomeController.get_daily_expense(data_date)
-        date_converted = date.strftime(outcome[0].date_created, "%d-%m-%Y")
-        message = f"**DAILY EXPENSE ON** {date_converted}\n"
+        if outcome:
+            date_converted = date.strftime(outcome[0].date_created, "%d-%m-%Y")
+            message = f"**DAILY EXPENSE ON** {date_converted}\n"
+        else:
+            message = "There's no expense on this day"
         for index, data in enumerate(outcome):
             message+= f"{index+1}. {data.detail_item} ({data.category.name}) Rp {data.amount}\n"
 
         return message
 
     async def get_monthly_expense(data_date):
-        outcome = await OutcomeController.get_monthly_expense(data_date)
-        date_converted = date.strftime(outcome[0].date_created, "%B")
+        next_month = data_date.replace(day=28) + timedelta(days=4)
+        next_month =  next_month - timedelta(days=next_month.day)
+        outcome = await OutcomeController.get_monthly_expense(data_date.replace(day=1),next_month)
+        if outcome:
+            date_converted = date.strftime(outcome[0].date_created, "%B")
+            message = f"**MONTHLY EXPENSE ON {date_converted}**\n"
+        else:
+            message = "There's no expense on this month"
         date_data = ""
-        message = f"**MONTHLY EXPENSE ON {date_converted}**\n"
         for index, data in enumerate(outcome):
             if date_data != data.date_created:
                 date_converted = date.strftime(data.date_created, "%d-%m-%Y")
