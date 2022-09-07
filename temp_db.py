cdecbbc9ac444df062f7a73a7e13a001f3671481
ZetsubouCode <kignatius80@gmail.com>
2022-09-08 06:52:30 +0700

    Make a temp place to store some data to minimize db lost connection

---
+++ temp_db.py
@@ -0,0 +1,14 @@
+class temp_db:
+    _category =[]
+    _income_type=[]
+
+    
+    # def get_category(self):
+    #     return self._category
+    # def set_category(self,array):
+    #     self._category = array
+        
+    # def get_income_type(self):
+    #     return self._category
+    # def set_income_type(self,array):
+    #     self._income_type = array
