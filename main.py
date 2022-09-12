5b1edae9385a171a85ab80b51c78aefb4fc2f1d8
ZetsubouCode <kignatius80@gmail.com>
2022-09-12 19:42:27 +0700

    Edit command name and resolve message limit issue

--- main.py
+++ main.py
@@ -1,17 +1,60 @@
-import os
+import os, sys, json
 import discord
 from discord.ext import commands
 from function.Command import Command as CommandFunction
 from temp_db import temp_db
 from _env import ENV
+# from nextcord import File, ButtonStyle, Embed, Color
+# from nextcord.ui import Button, View
 
 list_command = ['!help','!menu','!add_category','!add_outcome','!add_income',
                 '!add_budget_plan','!budget_plan','!add_saving_plan','!saving_plan',
                 '!add_limit_plan','!limit_plan','!add_limit','!limit',
-                '!daily_outcome', '!monthly_outcome', '!outcome_report', 
-                '!saving','!monthly_income','!budget','!remaining_money','!transfer']
+                '!daily_outcome', '!monthly_outcome', '!outcome_report','!monthly_income','!budget','!detail_budget','!transfer']
 
 client = commands.Bot(command_prefix='!',intents=discord.Intents.all())
+# client.remove_command("help")
+# helpGuide = json.load(open("help.json"))
+# # create help embed using page number and helpGuide
+# def createHelpEmbed(pageNum=0, inline=False):
+# 	pageNum = (pageNum) % len(list(helpGuide))
+# 	pageTitle = list(helpGuide)[pageNum]
+# 	embed=Embed(color=0x0080ff, title=pageTitle)
+# 	for key, val in helpGuide[pageTitle].items():
+# 		embed.add_field(name=client.command_prefix+key, value=val, inline=inline)
+# 		embed.set_footer(text=f"Page {pageNum+1} of {len(list(helpGuide))}")
+# 	return embed
+
+# @client.command(name="help")
+# async def Help(ctx):
+# 	currentPage = 0
+
+# 	# functionality for buttons
+
+# 	async def next_callback(interaction):
+# 		nonlocal currentPage, sent_msg
+# 		currentPage += 1
+# 		await sent_msg.edit(embed=createHelpEmbed(pageNum=currentPage), view=myview)
+
+# 	async def previous_callback(interaction):
+# 		nonlocal currentPage, sent_msg
+# 		currentPage -= 1
+# 		await sent_msg.edit(embed=createHelpEmbed(pageNum=currentPage), view=myview)
+
+# 	# add buttons to embed
+
+# 	previousButton = Button(label="<", style=ButtonStyle.blurple)
+# 	nextButton = Button(label=">", style=ButtonStyle.blurple)
+# 	previousButton.callback = previous_callback
+# 	nextButton.callback =  next_callback
+
+# 	myview = View(timeout=180)
+# 	myview.add_item(previousButton)
+# 	myview.add_item(nextButton)
+
+
+# 	sent_msg = await ctx.send(embed=createHelpEmbed(currentPage), view=myview)
+
 @client.event
 async def on_ready():
     category = await CommandFunction.get_category()
@@ -22,7 +65,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-#   try:
+  # try:
     if message.author == client.user or message.channel.name != 'bot-test':
         return
 
@@ -60,7 +103,9 @@ async def on_message(message):
         await message.channel.send(await CommandFunction.get_daily_outcome(client,message))
 
     if message.content.startswith('!monthly_outcome'):
-        await message.channel.send(await CommandFunction.get_monthly_outcome(client,message))
+        list_data = await CommandFunction.get_monthly_outcome(client,message)
+        for data in list_data:
+          await message.channel.send(data)
 
     if message.content.startswith('!outcome_report'):
         await message.channel.send(await CommandFunction.get_outcome_report(client,message))
@@ -74,10 +119,15 @@ async def on_message(message):
     if message.content =='!budget':
         await message.channel.send(await CommandFunction.get_remaining_money(client,message))
 
+    if message.content =='!detail_budget':
+        await message.channel.send(await CommandFunction.detail_budget(client,message))
+      
     if message.content =='!transfer':
         await message.channel.send(await CommandFunction.transfer(client,message))
-#   except Exception as e:
-#     await message.channel.send(f":warning::warning::warning:\n```Something wrong! ->{str(e)}```")
+  # except Exception as e:
+  #   exc_type, exc_obj, exc_tb = sys.exc_info()
+  #   fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+  #   await message.channel.send(f":warning::warning::warning:\n```Something wrong! ->{str(e)} | {fname}```")
 
 try:
   my_secret = ENV.TOKEN
