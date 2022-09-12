9b666331cd83e528bf2a65012c7ed1e4f66f6027
ZetsubouCode <kignatius80@gmail.com>
2022-09-12 19:46:56 +0700

    Fix param in add functioin, create new function

--- function/Category.py
+++ function/Category.py
@@ -1,13 +1,38 @@
 from controller.Category import Category as CategoryController
+from temp_db import temp_db
+
 class Category:
-    async def add(name):
+    async def add(msg):
         '''@param name = the name of the category'''
-        await CategoryController.add(name)
+        data = msg.split("#")
+        if len(data)==2:
+          name = data[0]
+          emot = ":"+data[1]+":"
+        elif len(data)==1:
+          name = data[0]
+          emot = None
+        else : 
+          return None
+        await CategoryController.add(name,emot)
+        return True
 
     async def get_all():
         category = await CategoryController.get_all()
         message = "**LIST CATEGORY\n**"
         for data in category:
+            if data.emoticon is None:
+                emot = ""
+            else :
+                emot = data.emoticon+" "
+            message += f"{data.id}. {emot}{data.name}\n"
+        return message
+
+    async def get_all_temp():
+        category = temp_db._category
+        message = "**LIST CATEGORY\n**"
+        print(category)
+        for data in category:
+            print(data)
             if data.emoticon is None:
                 emot = ""
             else :
