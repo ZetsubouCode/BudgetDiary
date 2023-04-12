2c9c33a6536842ca275f8c5978180800ab28cb50
Kevin Ignatius <53389426+ZetsubouCode@users.noreply.github.com>
2023-04-12 09:38:06 +0700

    Update Category.py
    Implement API request

--- function/Category.py
+++ function/Category.py
@@ -1,13 +1,26 @@
 from controller.Category import Category as CategoryController
 from temp_db import temp_db
+from util import Util
 
 class Category:
+    main_endpoint = "category"
+    
     async def add(name, emot):
-        await CategoryController.add(name,emot)
+        json_data = {
+        "name":name,
+         "emoticon":emot
+        }
+        sub_endpoint = "all"
+        category = Util.send_request('POST',main_endpoint,sub_endpoint,json=json_data)
         return True
 
+    def get_all_raw():
+        sub_endpoint = "all"
+        category = Util.send_request('GET',main_endpoint,sub_endpoint)
+        return CategoryController.get_all()    
+
     async def get_all():
-        category = await CategoryController.get_all()
+        category = await get_all_raw()
         message = "**LIST CATEGORY\n**"
         for data in category:
             if data.emoticon is None:
@@ -29,6 +42,3 @@ class Category:
                 emot = data.emoticon+" "
             message += f"{data.id}. {emot}{data.name}\n"
         return message
-
-    def get_all_raw():
-        return CategoryController.get_all()
