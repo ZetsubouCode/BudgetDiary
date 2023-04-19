b1ed9394957aa33d371da0675fa67690cf124727
Kevin Ignatius <53389426+ZetsubouCode@users.noreply.github.com>
2023-04-19 16:38:01 +0700

    Update IncomeType.py
    Implement API request

--- function/IncomeType.py
+++ function/IncomeType.py
@@ -1,15 +1,25 @@
 from controller.IncomeType import IncomeType as IncomeTypeController
+from util import Util
+
 class IncomeType:
-    async def add(name):
+    main_endpoint = "income_type"
+    
+    async def add(name:str):
         '''@param name = the name of the income_type'''
-        await IncomeTypeController.add(name)
+        sub_endpoint = "add"
+        form_data = {
+        "name":name
+        }
+        income_type = Util.send_request('POST',main_endpoint,sub_endpoint,data=form_data)
 
     async def get_all():
-        income_type = await IncomeTypeController.get_all()
+        income_type = await get_all_raw()
         message = "**LIST INCOME TYPE\n**"
-        for data in income_type:
-            message += f"{data.id}. {data.name}\n"
+        for idx,data in enumerate(income_type):
+            message += f"{idx}. {data.name}\n"
         return message
 
-    def get_all_raw():
-        return IncomeTypeController.get_all()
+    async def get_all_raw():
+        sub_endpoint = "all"
+        income_type = Util.send_request('GET',main_endpoint,sub_endpoint)
+        return income_type.content
