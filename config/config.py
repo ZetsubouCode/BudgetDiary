ccbba3c6cfb3b8247b2b22912615fa2faf89507b
ZetsubouCode <kignatius80@gmail.com>
2024-12-01 13:41:00 +0700

    add new path

---
+++ config/config.py
@@ -0,0 +1,6 @@
+import json, os
+
+JSON_CATEGORY_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data/category.json"))
+JSON_CATEGORY_TEMPLATE_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data/template/category.json"))
+JSON_USER_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data/user.json"))
+JSON_TRANSACTION_FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data/transaction/transaction.json"))
